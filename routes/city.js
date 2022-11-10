639f711ed1b1e57ec38eb6d742956438b7282953
joacogambra <joaquingambra@gmail.com>
2022-11-10 19:08:41 -0300

    routecity

--- routes/city.js
+++ routes/city.js
@@ -1,13 +1,12 @@
 let router = require('express').Router()
-const { get } = require('mongoose')
 let {create, read, one, update, destroy} = require('../controllers/city')
 
 router.route('/')
     .post(create)
     .get(read)
-router.route('/:id')
-    .get(one)
-    .put(update)
-    .delete(destroy)
+// router.route('/:id')
+//     .get(one)
+//     .put(update)
+//     .delete(destroy)
 
 module.exports = router;
