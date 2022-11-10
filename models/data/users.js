d5a568bc2465daa088a02191721ba53b7960e65d
joacogambra <joaquingambra@gmail.com>
2022-11-10 18:49:54 -0300

    datausers

--- models/data/users.js
+++ models/data/users.js
@@ -1,69 +1,73 @@
-let users = [
-    {
-        name: 'Ignacio',
-        lastName: 'Borraz',
-        role:"user",
-        photo:"https://avatars.githubusercontent.com/u/95113020?v=4",
-        age: 32,
-        email: 'ignacioborraz@hotmail.com',
-        password: 'Hola1234',
-        code: 32,
-        verified: true,
-        logged: true,
-    },
-    {
-        name: 'Lionel',
-        lastName: 'Messi',
-        role:"user",
-        photo:"https://img.a.transfermarkt.technology/portrait/big/28003-1631171950.jpg?lm=1",
-        age: 35,
-        email: 'leomessi10@hotmail.com',
-        password: 'thiagociromateo',
-        code: 10,
-        verified: true,
-        logged: true,
-    },
-    {
-        name: 'Anne',
-        lastName: 'Hathaway',
-        role:"user",
-        photo:"https://es.web.img2.acsta.net/pictures/19/10/16/01/22/0121805.jpg",
-        age: 39,
-        email: 'annehathaway@gmail.com',
-        password: 'diariodeunaprincesa',
-        code: 2000,
-        verified: true,
-        logged: true,
-    },
-    {
-        name: 'Julian',
-        lastName: 'Alvarez',
-        role:"user",
-        photo:"https://cdn.socy.cloud/PRAS/v1/_20/p/202206/upload/PRAS/uf_1654727957PD0AQwr15kapFkdiieXqae32Qt.jpg",
-        age: 22,
-        email: 'juli9@gmail.com',
-        password: 'riverplate33',
-        code: 9,
-        verified: true,
-        logged: true,
-    }
-]
+let users=[{
+  
+    name: "Eric",
+    lastName: "Rodriguez",
+    role:"adm",
+    photo:"https://i.pinimg.com/474x/18/b9/ff/18b9ffb2a8a791d50213a9d595c4dd52.jpg",
+    age: 23,
+    email: "feric.rodriguez@gmail.com",
+    password: "Chau6789",
+    code: "ryovxw2v",
+    verified: true, 
+    logged: true,
 
+},
+{
+    name: "Laila",
+    lastName: "Jalil",
+    role: "adm",
+    photo: "https://i.pinimg.com/474x/18/b9/ff/18b9ffb2a8a791d50213a9d595c4dd52.jpg",
+    age: 36,
+    email: "lailajalil@gmail.com",
+    password: "Aprobado2",
+    code: "aj7h57ow",
+    verified: true, 
+    logged: true,
+
+},
+{  
+    name: "Joaquin",
+    lastName: "Gambra",
+    role: "adm",
+    photo:"https://i.pinimg.com/474x/18/b9/ff/18b9ffb2a8a791d50213a9d595c4dd52.jpg",
+    age: 24,
+    email: "joaquingambra@gmail.com",
+    password: "Aprobado1",
+    code: "agkuu4yy",
+    verified: true, 
+    logged: true,
+},
+{
+
+    name: "Juan",
+    lastName: "Perez",
+    role: "adm",
+    photo:"https://i.pinimg.com/474x/18/b9/ff/18b9ffb2a8a791d50213a9d595c4dd52.jpg",
+    age: 40,
+    email: "feric.rodriguez@gmail.com",
+    password: "Chau1234",
+    code: "w8b6y3m2",
+    verified: true, 
+    logged: true,
+
+}
+
+]
 require('dotenv').config()
 require('../../config/database')
-const User = require('../User')
-
-users.forEach(e =>{
+const User= require('../User')
+users.forEach(elemento =>{
     User.create({
-        name:e.name,
-        lastName:e.lastName,
-        role:e.role,
-        photo:e.photo,
-        age:e.age,
-        email:e.email,
-        password:e.password,
-        code:e.code,
-        verified:e.verified,
-        logged:e.logged,
-    })
+    name: elemento.name,
+    lastName: elemento.lastName,
+    role: elemento.role,
+    photo: elemento.photo,
+    age: elemento.age,
+    email: elemento.email,
+    password: elemento.password,
+    code: elemento.code,
+    verified: elemento.verified,
+    logged: elemento.logged,
+})
+
 })
