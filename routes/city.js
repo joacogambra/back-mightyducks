6915e61fd09190f658ececa7023d145dffc9d872
joacogambra <joaquingambra@gmail.com>
2022-11-12 17:29:52 -0300

    routecity

--- routes/city.js
+++ routes/city.js
@@ -1,10 +1,11 @@
 let router = require('express').Router()
-let {create, read, one} = require('../controllers/city')
+let {create, read, one, update} = require('../controllers/city')
 
 router.route('/')
     .post(create)
     .get(read)
 router.get('/:id',one)
+router.put('/:id',update)
 // router.route('/:id')
 //    .get(one)
 //     .put(update)
