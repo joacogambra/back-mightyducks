673fcb5530605d55413b3a205db59733ad4d3f9f
joacogambra <joaquingambra@gmail.com>
2022-11-12 19:39:24 -0300

    routeitinerary

--- routes/itinerary.js
+++ routes/itinerary.js
@@ -1,9 +1,12 @@
 let router = require('express').Router()
-let {read, create, update} = require('../controllers/itineraries')
+let {read, create, update, destroy} = require('../controllers/itineraries')
 
 router.route('/')
     .post(create)
     .get(read)
 router.put('/:id',update)
+
+router.route('/:id')
+    .delete(destroy)
 
 module.exports = router;
