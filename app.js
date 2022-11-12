bc4a5524d02365ef553698d320bc2e1c802cc6a8
joacogambra <joaquingambra@gmail.com>
2022-11-12 18:45:21 -0300

    appjs

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors= require('cors');
 require('./config/database')
-const cors = require('cors')
 
 var indexRouter = require('./routes/index');
 // borro users router
@@ -18,7 +17,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
