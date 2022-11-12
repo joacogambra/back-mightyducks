7aabcd57a7b7bf3a4262f2b08888f1d5da88ba4a
joacogambra <joaquingambra@gmail.com>
2022-11-12 16:15:53 -0300

    routeindex

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ let router = require('express').Router()
 // cada modulo es un objeto
 let user = require('./user') //ubic de las rutas user
 let city = require('./city');
+let itinerary = require('./itinerary')
 // obligo al enrrutador principal que use "/user" para controlar las rutas de user
 router.use('/user',user)
 router.use('/cities',city)
+router.use('/itineraries',itinerary)
 
 module.exports = router;
 
