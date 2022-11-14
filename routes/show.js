7da3e55f1d89873f5108569c561c0b34f4942917
LailaJalil <lailajalil@gmail.com>
2022-11-14 12:18:59 -0300

    eliminar show

--- routes/show.js
+++ routes/show.js
@@ -1,11 +1,12 @@
 const router = require('express').Router()
 
-const { read , create, update }= require('../controllers/show')
+const { read , create, update, destroy }= require('../controllers/show')
 
 
 router.route('/')
 .get(read)
 .post(create)
-router.patch('/:id', update)    
+router.patch('/:id', update)   
+router.delete('/:id', destroy) 
 
 module.exports= router;
