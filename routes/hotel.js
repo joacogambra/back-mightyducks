56d33ace802aa054851880d4a9635d1dd51a693d
LailaJalil <lailajalil@gmail.com>
2022-11-14 01:04:22 -0300

    eliminar un hotel

--- routes/hotel.js
+++ routes/hotel.js
@@ -1,11 +1,12 @@
 const router = require('express').Router()
 
-const { create, read, one, update }= require('../controllers/hotel')
+const { create, read, one, update, destroy }= require('../controllers/hotel')
 
 router.route('/')
 .post(create)
 .get(read)
 router.get('/:id', one)
 router.patch('/:id', update)
+router.delete('/:id', destroy)
 
 module.exports= router;
