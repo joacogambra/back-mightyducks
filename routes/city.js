21db727dbc1d9167ac662536a770692ee6da8cd6
joacogambra <joaquingambra@gmail.com>
2022-11-21 12:24:17 -0300

    route city

--- routes/city.js
+++ routes/city.js
@@ -1,9 +1,13 @@
 let router = require('express').Router()
 let {create, read, one, update, destroy} = require('../controllers/city')
+let scheme = require('../schemes/schemeCity')
+let validator = require ('../middlewares/validator')
+const joi = require('joi')
 
 router.route('/')
-    .post(create)
+    .post(validator(scheme),create)
     .get(read)
+
 router.get('/:id',one)
 router.put('/:id',update)
 router.route('/:id')
