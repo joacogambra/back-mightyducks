6abbd31d04103cd27a872aaf041644b5c8e23759
joacogambra <joaquingambra@gmail.com>
2022-11-25 20:23:19 -0300

    middlewar account exist sign up

--- middlewares/accountExistsSignUp.js
+++ middlewares/accountExistsSignUp.js
@@ -1,12 +1,12 @@
-const Usuario = require("../models/User");
+const User = require("../models/User");
 const { userExistsResponse } = require("../config/responses");
 
-async function accountExists(req, res, next) {
-    const user = await Usuario.findOne({mail: req.body.mail})
+async function accountExistsSignUp(req, res, next) {
+    const user = await User.findOne({email: req.body.email})
     if (user) {
-        userExistsResponse(req,res)
+        return userExistsResponse(req,res)
     }
     return next()
 }
 
-module.exports =  accountExists 
+module.exports =  accountExistsSignUp 
