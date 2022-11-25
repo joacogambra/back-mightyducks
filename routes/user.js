cb6ee457cb67139cc69f961bd1fddf9948f8b47f
joacogambra <joaquingambra@gmail.com>
2022-11-25 20:23:52 -0300

    route user

--- routes/user.js
+++ routes/user.js
@@ -1,21 +1,25 @@
 //primer paso de cualquier enrrutador, es requerir el metodo router de express
 let router = require('express').Router()
 let schema = require('../schemas/userSignIn')
+let scheme = require('../schemas/userSignUp')
 let validator= require('../middlewares/validator')
 let accountExistsSignIn = require('../middlewares/accountExistsSignIn')
+let accountExistsSignUp = require('../middlewares/accountExistsSignUp')
 let accountHasBeenVerified = require('../middlewares/accountHasBeenVerified')
 let mustsignin = require('../middlewares/mustsignin')
 let passport= require('../config/passport')
 
 // desestructuro para traer los metodos que necesito enrrutar
-let {create, read, update,destroy, signUp, signIn, verify, signInWithToken} = require('../controllers/user')
+let { signUp, signIn, verify, signInWithToken} = require('../controllers/user')
 
 //utilizo el metodo route para agregar a la ruta
 //concateno todas las palabras y obtengo la ruta total
 // para controlar este metodo
-router.post('/')
+// router.post('/')
 
 router.get('/verify/:code', verify)
+
+router.post('/sign-up',validator(scheme),accountExistsSignUp,signUp)
 router.post('/sign-in', validator(schema), accountExistsSignIn, accountHasBeenVerified, signIn)
 router.post('/token',passport.authenticate("jwt",{session:false}), mustsignin, signInWithToken)
 // router.route('/').post(create)
