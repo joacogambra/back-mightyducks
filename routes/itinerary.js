68048b57ea9c9f023d021444a70daeff867d09d4
joacogambra <joaquingambra@gmail.com>
2022-11-27 22:13:10 -0300

    itinerary rout

--- routes/itinerary.js
+++ routes/itinerary.js
@@ -1,12 +1,17 @@
 let router = require('express').Router()
 let {read, create, update, destroy} = require('../controllers/itineraries')
+let schema = require('../schemas/userItinerary')
+let validator = require('../middlewares/validator')
+let passport = require('../config/passport')
+
+
+router.post('/', passport.authenticate('jwt', { session: false }), validator(schema), create)
+router.put('/:id',passport.authenticate('jwt', { session: false }),update)
 
 router.route('/')
-    .post(create)
     .get(read)
-router.put('/:id',update)
-
 router.route('/:id')
     .delete(destroy)
+
 
 module.exports = router;
