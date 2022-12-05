2fa64bacf8687c45ad3a6b9a867dc98744367834
LailaJalil <lailajalil@gmail.com>
2022-12-05 18:17:04 -0300

    arreglos

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -1,65 +1,82 @@
 const { response } = require('../app')
-const Hotel = require('../models/Hotel')
+const Hotel= require('../models/Hotel')
 
 const controller = {
-    create: async (req, res) => {
-        try {
-            let new_hotel = await Hotel.create(req.body)
-            res.status(201).json({
-                response: new_hotel,
-                success: true,
-                message: "The Hotel was created successfully"
-            })
-        }
-        catch (error) {
-            res.status(400).json({
-                success: false,
-                message: error.message
-            })
-        }
-    },
-    read: async (req, res) => {
-        let query = {}
-        let order = {}
-        let id = {}
-        try {
-            if (req.query.name) {
-                query = { name: { $regex: `${req.query.name}`, $options: 'i' } }
 
-            }
-            if (req.query.order) {
-                order = { capacity: req.query.order }
-            }
-            if (req.query.userId) {
-                query = { userId: req.query.userId }
-            }
-            let hotels = await Hotel.find(query, '-userId').sort(order)
-            if (hotels.length > 0) {
-                res.status(200).json({
-                    response: hotels,
-                    success: true,
-                    message: "hotels are filtered successfully"
-                })
-            }
-            else {
-                res.status(404).json({
-                    response: res.message,
-                    message: 'Comment Could Not Be Created',
-                    succes: false,
-                })
-            }
-        }
-        catch (error) {
-            res.status(400).json({
-                success: false,
-                message: error.message
-            })
-        }
+    create: async (req, res)=>{
+    
+    try{ 
+      let new_hotel = await Hotel.create(req.body)
+      
+        res.status(201).json({
+            response: new_hotel,
+            success: true, 
+             message: "The Hotel was created successfully"
+        })
+
+    }
+    
+    catch(error){
+        res.status(400).json({
+            success:false,
+            message: error.message
+        })
+     }
+    
     },
-    one: async (req, res) => {
-        let { id } = req.params
+
+  read: async (req, res)=>{
+    let query= {}
+    let order= {}
+    let id = {}
+    
+    try{ 
+        if (req.query.name){
+            query={ name: {$regex:`${req.query.name}`, $options:'i' }}
+            
+        } 
+        // regex es un operador que sirve para buscar palabras equivalentes, options i es 
+                //para que los dos esten en lowercase and
+        if (req.query.order){
+           
+        order={ capacity: req.query.order} 
+    
+        }
+        if (req.query.userId){
+            query= { userId: req.query.userId}
+        }
+      let hotels = await Hotel.find(query, '-userId').sort(order)
+
+      if (hotels.length>0){
+        res.status(200).json({
+            response: hotels,
+            success: true,     
+            message: "hotels are filtered successfully"
+        })
+    } 
+    
+    else{
+     
+        res.status(404).json({
+            response: res.message,
+            message: 'Comment Could Not Be Created',
+            succes: false,
+        })
+
+    }}
+    catch(error){
+        res.status(400).json({
+            success:false,
+            message: error.message
+        })
+     }
+    
+    },   
+    one: async(req,res) => { 
+         let { id } = req.params
+       
         try {
-            let hotels = await Hotel.findById(id).populate({ path: "userId", select: "name photo -_id" })
+            let hotels = await Hotel.findById(id).populate({path:"userId",  select: "name photo -_id"} )
             if (hotels) {
                 res.status(200).json({
                     response: hotels,
@@ -71,19 +88,20 @@ const controller = {
                     success: false,
                     message: "Couldn't find hotel"
                 })
-            }
-        } catch (error) {
+            }            
+        } catch(error) {
             res.status(400).json({
                 success: false,
                 message: error.message
             })
-        }
+        }        
     },
-    update: async (req, res) => {
+    update: async(req,res)=>{
         let update = req.body
         let { id } = req.params
+        
         try {
-            let hotels = await Hotel.findOneAndUpdate(id, update, { new: true })
+            let hotels = await Hotel.findOneAndUpdate(id, update, {new:true})
             if (hotels) {
                 res.status(200).json({
                     response: hotels,
@@ -95,17 +113,21 @@ const controller = {
                     success: false,
                     message: "Couldn't update hotel"
                 })
-            }
-        } catch (error) {
+            }            
+        } catch(error) {
             res.status(400).json({
                 success: false,
                 message: error.message
             })
-        }
+        }        
+
+
     },
-    destroy: async (req, res) => {
+    destroy: async (req, res)=>{
         let remove = req.body
         let { id } = req.params
+        
+    
         try {
             let hotels = await Hotel.findByIdAndRemove(id, remove)
             if (hotels) {
@@ -119,14 +141,14 @@ const controller = {
                     success: false,
                     message: "Couldn't remove hotel"
                 })
-            }
-        } catch (error) {
+            }            
+        } catch(error) {
             res.status(400).json({
                 success: false,
                 message: error.message
             })
-        }
+        }   
 
-    }
+}
 }
 module.exports = controller;
